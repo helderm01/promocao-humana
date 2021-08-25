9ee62c7ccf2207ea856cf807dc57e0d70c32cf9b
helder <helderm01@gmail.com>
2021-08-24 21:37:37 -0300

    Tela de cadastro de família

--- src/PromocaoHumana.Web/PromocaoHumana.Web/Controllers/FamiliaController.cs
+++ src/PromocaoHumana.Web/PromocaoHumana.Web/Controllers/FamiliaController.cs
@@ -1,14 +1,40 @@
 using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PromocaoHumana.Web.Data;
 using PromocaoHumana.Web.Models.Familia;
 
 namespace PromocaoHumana.Web.Controllers
 {
+    [Authorize]
     public class FamiliaController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public FamiliaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var familias = _db.Familias.ToList();
+            var retorno = familias.Select(c => new FamiliaViewModel()
+            {
+                Id = c.Id,
+                NomeResponsavel = c.NomeResponsavel,
+                CpfResponsavel = c.CpfResponsavel,
+                NomeConjuge = c.NomeConjuge,
+                QuantidadeFilhos = c.QuantidadeFilhos
+            });
+
+            return View(retorno);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new FamiliaViewModel());
         }
 
         public IActionResult Salvar(FamiliaViewModel familiaViewModel)
--- src/PromocaoHumana.Web/PromocaoHumana.Web/Models/Familia/FamiliaViewModel.cs
+++ src/PromocaoHumana.Web/PromocaoHumana.Web/Models/Familia/FamiliaViewModel.cs
@@ -8,29 +8,27 @@ namespace PromocaoHumana.Web.Models.Familia
     public class FamiliaViewModel
     {
         public int Id { get; set; }
-        
-        [ReadOnly(true)]
-        public DateTime DataCadastro { get; set; }
-        
-        [ReadOnly(true)]
-        public bool Ativa { get; set; }
-        
+
+        [ReadOnly(true)] public DateTime DataCadastro { get; set; } = DateTime.Today;
+
+        [ReadOnly(true)] public bool Ativa { get; set; } = true;
+
         [Required(ErrorMessage = "{0} é obrigatório.")]
         [StringLength(150, ErrorMessage = "{0} não pode ter mais que {1} caracteres.")]
         public string NomeResponsavel { get; set; }
-        
+
         [Required(ErrorMessage = "{0} é obrigatório.")]
         [StringLength(15, ErrorMessage = "{0} não pode ter mais que {1} caracteres.")]
         public string CpfResponsavel { get; set; }
-        
+
         [StringLength(150, ErrorMessage = "{0} não pode ter mais que {1} caracteres.")]
         public string NomeConjuge { get; set; }
-        
+
         public int QuantidadeFilhos { get; set; }
-        
+
         [Required(ErrorMessage = "{0} é obrigatório.")]
         public int ParoquiaId { get; set; }
-        
+
         [Required(ErrorMessage = "{0} é obrigatório.")]
         public EnderecoViewModel Endereco { get; set; }
     }
