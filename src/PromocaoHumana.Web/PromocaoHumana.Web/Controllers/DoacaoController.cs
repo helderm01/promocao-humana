0353d0b5436abb48f74a30d43510aa35cf084cbd
Helder Souza <helder.souza@pagueveloz.com.br>
2021-08-27 23:49:43 -0300

    Criação da View de Doações

---
+++ src/PromocaoHumana.Web/PromocaoHumana.Web/Controllers/DoacaoController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PromocaoHumana.Web.Data;
+using PromocaoHumana.Web.Models.Doacao;
+
+namespace PromocaoHumana.Web.Controllers
+{
+    [Authorize]
+    public class DoacaoController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DoacaoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var doacoes = _db.Doacoes.ToList();
+            var retorno = doacoes.Select(c => new DoacaoViewModel
+            {
+                Id = c.Id,
+                Descricao = c.Descricao,
+                FamiliaId = c.FamiliaId,
+                LocalRetirada = c.LocalRetirada.Nome,
+                MesRetirada = c.MesRetirada,
+                QuemRetirou = c.QuemRetirou,
+                ResponsavelFamilia = c.Familia.NomeResponsavel,
+                LocalRetiradaId = c.LocalRetiradaId
+            });
+
+            return View(retorno);
+        }
+
+        // public IActionResult Create()
+        // {
+        //     return View(new DoacaoViewModel());
+        // }
+    }
+}
--- src/PromocaoHumana.Web/PromocaoHumana.Web/Models/Doacao/DoacaoViewModel.cs
+++ src/PromocaoHumana.Web/PromocaoHumana.Web/Models/Doacao/DoacaoViewModel.cs
@@ -1,7 +1,26 @@
-﻿namespace PromocaoHumana.Web.Models.Doacao
+﻿using System.ComponentModel;
+
+namespace PromocaoHumana.Web.Models.Doacao
 {
     public class DoacaoViewModel
     {
+        public int Id { get; set; }
         
+        [DisplayName("Mês retirada")]
+        public string MesRetirada { get;  set; }
+        
+        [DisplayName("Descrição")]
+        public string Descricao { get;  set; }
+        
+        [DisplayName("Família")]
+        public string ResponsavelFamilia { get;  set; }
+        public int? FamiliaId { get; set; }
+        
+        [DisplayName("Local")]
+        public string LocalRetirada { get;  set; }
+        public int? LocalRetiradaId { get;  set; }
+        
+        [DisplayName("Quem retirou?")]
+        public string QuemRetirou { get;  set; }
     }
 }
